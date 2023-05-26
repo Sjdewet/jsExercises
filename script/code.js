9309eec7db296957448b1d4e37a3fa143d4a7e3b
Sjdewet <summerdewet@gmail.com>
2023-05-26 11:34:22 +0200

    sjd

--- script/code.js
+++ script/code.js
@@ -1,24 +1,46 @@
+// Q1
 // let fullName= "Summer De Wet";
 // console.log(fullName);
 
-function fullName (){
-    let  firstName ="Summer";
-    let  surname ="De Wet";
-     return firstName + " " +surname
- }
- console.log(fullName());
- function age (){
-     let myAge = "18";
-     return myAge
- }
- console.log(age());
- function subjects(){
-     let array =["English","Mathemathics","Afrikaans"]
-     return array
- }
- console.log(subjects());
- function living(){
-     let myAddress =["Southfield","Flintdale Road","South Africa"]
-     return myAddress
- }
- console.log(living());
+// Q2
+// function fullName (){
+//     let  firstName ="Summer";
+//     let  surname ="De Wet";
+//      return firstName + " " +surname
+//  }
+//  console.log(fullName());
+//  function age (){
+//      let myAge = "18";
+//      return myAge
+//  }
+//  console.log(age());
+//  function subjects(){
+//      let array =["English","Mathemathics","Afrikaans"]
+//      return array
+//  }
+//  console.log(subjects());
+//  function living(){
+//      let myAddress =["Southfield","Flintdale Road","South Africa"]
+//      return myAddress
+//  }
+//  console.log(living());
+
+// Q3
+
+function getSum(){
+    let num1 = +document.querySelector('#number1').value
+    let num2 = +document.querySelector('#number2').value
+    let operator = document.querySelector('#operator').value
+    let display = document.querySelector('#screen')
+
+    if(operator == '+'){
+        display.textContent = num1 + num2
+    } else if(operator == '-'){
+        display.innerText = num1 - num2
+    } else if(operator == '*'){
+        display.innerText = num1 * num2
+    } else if(operator == '/'){
+        display.innerText = num1 / num2
+    }
+}
+
