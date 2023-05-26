cfbaae9a61e64e62ec04b4b2bf7d2d6617f7ea5f
Sjdewet <summerdewet@gmail.com>
2023-05-26 09:18:28 +0200

    summer

--- script/code.js
+++ script/code.js
@@ -1,2 +1,24 @@
-let fullName= "Summer De Wet";
-console.log(fullName);
+// let fullName= "Summer De Wet";
+// console.log(fullName);
+
+function fullName (){
+    let  firstName ="Summer";
+    let  surname ="De Wet";
+     return firstName + " " +surname
+ }
+ console.log(fullName());
+ function age (){
+     let myAge = "18";
+     return myAge
+ }
+ console.log(age());
+ function subjects(){
+     let array =["English","Mathemathics","Afrikaans"]
+     return array
+ }
+ console.log(subjects());
+ function living(){
+     let myAddress =["Southfield","Flintdale Road","South Africa"]
+     return myAddress
+ }
+ console.log(living());
